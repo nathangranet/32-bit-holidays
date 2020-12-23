129d56a21809136663139cbb6a4b599c54284ba0
gaetan <gaetan.depin@epitech.eu>
2020-12-23 11:03:28 +0100

    add score

--- scene/Game.gd
+++ scene/Game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const PIPE_CLASS = preload("res://scene/Pipe.tscn")
+var score = -3 setget set_score
 
 
 func _ready():
@@ -16,8 +17,18 @@ func _on_SpawnWallTimer_timeout():
 	var pipe = PIPE_CLASS.instance()
 	pipe.position = Vector2(1200, y)
 	self.add_child(pipe)
+	self.score += 1
 
 
 func _on_Player_game_ended():
 	$Node2D/Replay.show()
 	get_tree().set_pause(true)
+
+
+func set_score(value):
+	score = value
+	if $Label:
+		if value >= 0:
+			$Label.text = String(value)
+		else:
+			$Label.text = "0"
